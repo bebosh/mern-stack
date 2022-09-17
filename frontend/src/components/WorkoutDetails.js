9f12dead35db28b6a85fa1e5047a42a19b0a03e9
Paolo <pieffelleo@gmail.com>
2022-09-17 11:11:19 +0200

    mern stack app finish

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import React from 'react';
 
-export const WorkoutDetails = ({workout}) => {
+// import date-fns 
+
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
+export const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch('api/workouts/' + workout._id, {
+      method: 'DELETE',
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_WORKOUT', payload: json });
+    }
+  };
   return (
-    <div className='workout-details' >
+    <div className="workout-details">
       <h4>{workout.title}</h4>
-      <p> <strong>Load:</strong> {workout.load}</p>
-      <p> <strong>Reps:</strong> {workout.reps}</p>
-      <p>{workout.createdAt}</p>
+      <p>
+        <strong>Load:</strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps:</strong> {workout.reps}
+      </p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix:true})}</p>
+      <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
     </div>
-  )
-}
+  );
+};
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 export const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
   const [title, setTitle] = useState('');
   const [load, setLoad] = useState('');
   const [reps, setReps] = useState('');
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,13 +23,16 @@ export const WorkoutForm = () => {
     const json = await response.json();
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
       setTitle('');
       setLoad('');
       setReps('');
       setError(null);
+      setEmptyFields([]);
       console.log('new workout added', json);
+      dispatch({ type: 'CREATE_WORKOUT', payload: json });
     }
   };
 
@@ -38,22 +44,25 @@ export const WorkoutForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
       />
       <label>Loads (kg):</label>
       <input
         type="number"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
       />
       <label> Reps:</label>
       <input
         type="number"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
       />
 
       <button>Add workout</button>
-      {error && <div className="error">{error}</div>}
+      {error && <div className="error">{error +" " +emptyFields}</div>}
     </form>
   );
 };
